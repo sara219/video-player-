f3ea4fee1155c97fb1cfb2f35c9892dafa5e3179
Sara <sara97abuawwad@gmail.com>
2020-06-12 16:47:52 +0300

    Global Styling

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Player from './Player'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import GlobalStyle from '../styles/GlobalStyle'
 
 const App = () => (
   <BrowserRouter>
+  <>
     <Switch>
       <Route exact path='/' component={Player} />
       <Route exact path='/:activeVideo' component={Player} />
     </Switch>
+    <GlobalStyle />
+  </>
   </BrowserRouter>
 )
 
