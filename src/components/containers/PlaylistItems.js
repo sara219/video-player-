5e3592f4ee6e145558958f51829819aec4d0bfde
Sara <sara97abuawwad@gmail.com>
2020-06-12 20:37:26 +0300

    Component Styling

--- src/components/containers/PlaylistItems.js
+++ src/components/containers/PlaylistItems.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import PlaylistItem from '../PlaylistItem'
+import StyledPlayListItem from '../styles/StyledPlaylistItem'
 
 
 const PlaylistItems = props => (
+     <StyledPlayListItem>
      <PlaylistItem />
+     </StyledPlayListItem>
 )
 
 export default PlaylistItems
