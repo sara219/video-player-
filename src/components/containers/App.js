9acb7b7267d39b5276e6cd038b9c5dec08b6cb2f
Sara <sara97abuawwad@gmail.com>
2020-06-12 16:26:40 +0300

    setting up the routes

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Player from './Player'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 const App = () => (
-  <Player />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Player} />
+      <Route exact path='/:activeVideo' component={Player} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App;
